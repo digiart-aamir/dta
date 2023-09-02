7b78b8e795bfb356ad6801ffa86d9f1a0991ac62
Aamir Jamil <digiart.aamir@gmail.com>
2023-09-02 14:13:21 +0500

    dashboard startup

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,111 @@
 <?php
-  include "session.php";
-
-  echo "hello";
-  echo $fname;
-  echo 'your email is ';
-  echo $email;
+  include "session.php"; 
  ?>
  
- 
- 
- 
- <h1>hello</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/dashboard.css">
+</head>
+<body>
+<header>
+    <nav>
+      <h1>logo</h1>
+      <ul class="nav">
+        <button onclick="showPage('home')">Popular threads</button>
+        <button onclick="showPage('about')">new threads</button>
+        <button onclick="showPage('contact')">your thread</button>
+        <button onclick="showPage('contact')">add thread</button>
+        <button onclick="showPage('contact')">profile</button>
+        <button onclick="showPage('contact')">logout</button>
+      </ul>
+    </nav>
+</header>	
 
+  <main>
+
+
+  <section id="home">
+    <h1>Home</h1>
+    <p>Home page content goes here...</p>
+  </section>
+
+  <section id="about">
+    <h1>About</h1>
+    <p>About page content goes here...</p>
+  </section>
+
+  <section id="contact">
+    <h1>Contact</h1>
+    <p>Contact page content goes here...</p>
+  </section>
+
+</main>
+
+<script>
+  function showPage(pageId) 
+  {
+
+    // Hide all sections
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('about').style.display = 'none';
+    document.getElementById('contact').style.display = 'none';
+
+    // Show selected section
+    document.getElementById(pageId).style.display = 'block';
+
+
+    let activePage = 'home';
+
+    function showPage(page) 
+    {
+      document.querySelector('.nav .active')?.classList.remove('active');
+      document.querySelector(`.nav button[onclick="showPage('${page}')"]`).classList.add('active');
+      
+      activePage = page;
+      
+      // Logic to show/hide pages
+      
+      console.log('Showing page:', activePage); 
+    }
+
+  }
+
+  // Default to show home page on load
+  showPage('home');
+</script>
+
+  
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+ 
+ 
+<?php
+    // echo "ID: $id <br>";
+    // echo "First Name: $fname <br>"; 
+    // echo "Last Name: $lname <br>";
+    // echo "email $email <br>";
+    // echo "Age: $age <br>";
+    // echo "Category: $category <br>";
+    // echo "Field: $field";
+
+    // foreach ($topics as $topic)
+    // {
+    //   echo $topic;
+    //   echo '<br>';
+    // }
+
+
+?>
--- session.php
+++ session.php
@@ -17,15 +17,31 @@
     // Query the database for the user data
     $query = "SELECT * FROM users WHERE email = '$email'";
     $result = mysqli_query($conn, $query);
+    
 
     // Fetch the user data
     $user = mysqli_fetch_assoc($result);
 
     // Access the user data
-    
+    $id = $user['id'];
     $fname = $user['fname'];
     $lname = $user['lname'];
-    $email = $user['email'];
+    $age= $user['age'];
+    $category = $user['Category'];
+    $field = $user['Field'];
+
+    $query2 = "SELECT name FROM topics WHERE user_id = '$id'";
+    $result2 = mysqli_query($conn, $query2);
+
+    $topics = array();
+
+    while($row = mysqli_fetch_assoc($result2)) 
+    {
+      $topics[] = $row['name']; 
+    }
+
+
+
     // ...
 
 ?>
